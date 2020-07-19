ee1912b13782fa0a9411fdf81be3140f1dff0448
zingimmick <zingimmick@outlook.com>
2020-07-19 10:30:30 +0800

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 wget https://github.com/symplify/easy-coding-standard-prefixed/archive/$1.tar.gz -O ecs.tar.gz \
+    && tar -xvf ecs.tar.gz && ls \
     && tar --strip-components 1 -xvf ecs.tar.gz easy-coding-standard-prefixed-$1/ecs \
     && chmod a+x ecs \
     && mv ecs /usr/local/bin/ecs
