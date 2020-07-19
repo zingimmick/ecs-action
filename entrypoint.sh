85068c0e8d8fae86a751095f8208a2924bb61a07
zingimmick <zingimmick@outlook.com>
2020-07-19 08:12:42 +0800

    Add the version input variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -l
 
 set -e
+
+wget https://github.com/symplify/easy-coding-standard-prefixed/raw/v8.1.6/ecs.phar -O ecs \
+    && chmod a+x ecs \
+    && mv ecs /usr/local/bin/ecs
+
 /usr/local/bin/ecs check $*
